26ad19fc13415468bab6a344970dd60bbfcce51c
Zeeta:) <113794999+z3t444@users.noreply.github.com>
2024-03-23 13:23:55 +0700

    <ui>(Testing UI)

--- app/account/account-form.tsx
+++ app/account/account-form.tsx
@@ -1,76 +1,63 @@
-'use client'
-import { useCallback, useEffect, useState } from 'react'
-import Avatar from './avatar'
-import { Database } from '../database.types'
-import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+"use client";
+import { useCallback, useEffect, useState } from "react";
+import Avatar from "./avatar";
+import { Database } from "../database.types";
+import { Session, createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default function AccountForm({ session }: { session: Session | null }) {
-  const supabase = createClientComponentClient<Database>()
-  const [loading, setLoading] = useState(true)
-  const [fullname, setFullname] = useState<string | null>(null)
-  const [username, setUsername] = useState<string | null>(null)
-  const [website, setWebsite] = useState<string | null>(null)
-  const [avatar_url, setAvatarUrl] = useState<string | null>(null)
-  const user = session?.user
+  const supabase = createClientComponentClient<Database>();
+  const [loading, setLoading] = useState(true);
+  const [fullname, setFullname] = useState<string | null>(null);
+  const [username, setUsername] = useState<string | null>(null);
+  const [website, setWebsite] = useState<string | null>(null);
+  const [avatar_url, setAvatarUrl] = useState<string | null>(null);
+  const user = session?.user;
 
   const getProfile = useCallback(async () => {
     try {
-      setLoading(true)
+      setLoading(true);
 
-      let { data, error, status } = await supabase
-        .from('profiles')
-        .select(`full_name, username, website, avatar_url`)
-        .eq('id', user?.id)
-        .single()
+      let { data, error, status } = await supabase.from("profiles").select(`full_name, username, website, avatar_url`).eq("id", user?.id || "").single();
 
       if (error && status !== 406) {
-        throw error
+        throw error;
       }
 
       if (data) {
-        setFullname(data.full_name)
-        setUsername(data.username)
-        setWebsite(data.website)
-        setAvatarUrl(data.avatar_url)
+        setFullname(data.full_name);
+        setUsername(data.username);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
       }
     } catch (error) {
-      alert('Error loading user data!')
+      alert("Error loading user data!");
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }, [user, supabase])
+  }, [user, supabase]);
 
   useEffect(() => {
-    getProfile()
-  }, [user, getProfile])
+    getProfile();
+  }, [user, getProfile]);
 
-  async function updateProfile({
-    username,
-    website,
-    avatar_url,
-  }: {
-    username: string | null
-    fullname: string | null
-    website: string | null
-    avatar_url: string | null
-  }) {
+  async function updateProfile({ username, website, avatar_url }: { username: string | null; fullname: string | null; website: string | null; avatar_url: string | null }) {
     try {
-      setLoading(true)
+      setLoading(true);
 
-      let { error } = await supabase.from('profiles').upsert({
+      let { error } = await supabase.from("profiles").upsert({
         id: user?.id as string,
         full_name: fullname,
         username,
         website,
         avatar_url,
         updated_at: new Date().toISOString(),
-      })
-      if (error) throw error
-      alert('Profile updated!')
+      });
+      if (error) throw error;
+      alert("Profile updated!");
     } catch (error) {
-      alert('Error updating the data!')
+      alert("Error updating the data!");
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
   }
 
@@ -81,8 +68,8 @@ export default function AccountForm({ session }: { session: Session | null }) {
         url={avatar_url}
         size={150}
         onUpload={(url) => {
-          setAvatarUrl(url)
-          updateProfile({ fullname, username, website, avatar_url: url })
+          setAvatarUrl(url);
+          updateProfile({ fullname, username, website, avatar_url: url });
         }}
       />
       <div>
@@ -91,39 +78,20 @@ export default function AccountForm({ session }: { session: Session | null }) {
       </div>
       <div>
         <label htmlFor="fullName">Full Name</label>
-        <input
-          id="fullName"
-          type="text"
-          value={fullname || ''}
-          onChange={(e) => setFullname(e.target.value)}
-        />
+        <input id="fullName" type="text" value={fullname || ""} onChange={(e) => setFullname(e.target.value)} />
       </div>
       <div>
         <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={username || ''}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+        <input id="username" type="text" value={username || ""} onChange={(e) => setUsername(e.target.value)} />
       </div>
       <div>
         <label htmlFor="website">Website</label>
-        <input
-          id="website"
-          type="url"
-          value={website || ''}
-          onChange={(e) => setWebsite(e.target.value)}
-        />
+        <input id="website" type="url" value={website || ""} onChange={(e) => setWebsite(e.target.value)} />
       </div>
 
       <div>
-        <button
-          className="button primary block"
-          onClick={() => updateProfile({ fullname, username, website, avatar_url })}
-          disabled={loading}
-        >
-          {loading ? 'Loading ...' : 'Update'}
+        <button className="button primary block" onClick={() => updateProfile({ fullname, username, website, avatar_url })} disabled={loading}>
+          {loading ? "Loading ..." : "Update"}
         </button>
       </div>
 
@@ -135,5 +103,5 @@ export default function AccountForm({ session }: { session: Session | null }) {
         </form>
       </div>
     </div>
-  )
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
-import AuthForm from './auth-form'
+import AuthForm from "./auth-form";
+import './page.css'
 
 export default function Home() {
   return (
     <div className="row">
       <div className="col-6">
-        <h1 className="header">Supabase Auth + Storage</h1>
-        <p>
-          Experience our Auth and Storage through a simple profile management example. Create a user
-          profile and upload an avatar image. Fast, simple, secure.
-        </p>
+        <h1 className="header">Hello, Welcomeback to Whatever UIkit</h1>
+        <p className="description">we provide many different types of UIkit section Website. if you are confident with our application, lets login in the forum on the right by entering a Gmail or Email account.</p>
       </div>
       <div className="col-6 auth-widget">
         <AuthForm />
       </div>
     </div>
-  )
+  );
 }
