2e24b5fd6f61087cfce3981cb96868b9d25368f7
Zeeta:) <113794999+z3t444@users.noreply.github.com>
2024-04-23 19:28:47 +0700

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ export default function Home() {
   return (
     <>
       {/* for auth Forms */}
-      <div className="grid md:gird lg:flex xl:flex items-center mt-40 mb-24 justify-between">
-        <div className="col-6">
-          <h1 className="header text-2xl font-medium"></h1>
-          <p className="description mt-3 Archivo text-sm leading-7"></p>
+      <div className="grid md:gird lg:flex xl:flex items-center mt-40 mb-24 justify-center">
+        <div className="max-w-2xl">
+          <h1 className="header text-5xl text-gray-200 font-medium">Whatever</h1>
+          <p className="mt-3 text-gray-200 Archivo text-sm leading-7">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim, omnis dolorem deserunt nam hic possimus eius nobis voluptates reiciendis sed.</p>
         </div>
-        <div id="authForm" className="col-6 auth-widget">
+        <div id="authForm" className="auth-widget">
           <AuthForm />
         </div>
       </div>
